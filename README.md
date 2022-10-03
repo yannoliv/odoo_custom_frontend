9203fee5f11b599b80afc6ac8a807e03eedd8461
yannoliv <yannick.olivier@hotmail.com>
2022-10-03 09:56:48 +0200

    Step 2

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Een custom frontend bouwen voor odoo: stap 1.
 Zorg ervoor dat postgres aan het runnen is, dit is normaal het geval.
 De server opstarten doe je via
 flask --app main run
+
+
+## 2. Even wat css.
+
+https://tailwindui.com/components
+
+Ik heb hier letterlijk enkel wat tailwindcss op aan gegooid en kijk hoe mooi het er al uitziet.
+Templating gebruikt gewoon jinja zoals we kennen vanuit odoo en django.
--- main.py
+++ main.py
@@ -1,21 +1,37 @@
-from flask import Flask
+"""
+Simple module to show how to connect to a odoo database
+"""
+
+
+from flask import Flask, render_template
 import psycopg2
+import psycopg2.extras
 from config import config
 
 app = Flask(__name__)
 
 
-def connect():
+def connect() -> list[dict]:
+    """
+    Database connect
+    :return: List of dictionaries of the objects
+    """
     params = config()
     conn = psycopg2.connect(**params)
-    cur = conn.cursor()
-    cur.execute('SELECT * from product_template')
-    line = cur.fetchone()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute("""
+        SELECT name,quantity,id 
+        FROM product_template 
+        WHERE quantity IS NOT NULL
+        LIMIT 30
+    """)
+    res = [dict(record) for record in cur]
     cur.close()
-    return line
+    return res
+
 
 @app.route("/")
 def index():
-    line = connect()
-
-    return f'{line}'
+    res = connect()
+    # /web/image?model=product.template&id=85&field=image_256
+    return render_template('index.html', lines=res)
